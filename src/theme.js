1343e1e6fe98e9837897b44b9dd4245cdaf9b27d
Marcos V. Silva <mrcsvs@outlook.com>
2022-12-07 18:41:38 -0300

    estrutura inicial + material ui

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+  },
+});
+
+export default theme;
