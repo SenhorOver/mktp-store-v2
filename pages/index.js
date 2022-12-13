c8fe393bbc0a505a4889677eb1844718df9ed352
Marcos V. Silva <mrcsvs@outlook.com>
2022-12-13 18:54:01 -0300

    header - add container and user menu

---
+++ pages/index.js
@@ -0,0 +1,96 @@
+import {
+    Card,
+    CardMedia,
+    CardContent,
+    IconButton,
+    InputBase,
+    Typography,
+    Container,
+    Paper,
+    Grid 
+} from "@mui/material"
+import { Search } from "@mui/icons-material"
+import TemplateDefault from "../src/templates/Default"
+import theme from "../src/theme"
+
+const Home = () => {
+    return (
+        <TemplateDefault>
+            <Container disableGutters maxWidth='md' sx={{ padding: theme.spacing(8, 10, 6) }}>
+                <Typography component='h1' variant="h3" align="center" color='textPrimary'>
+                    O que deseja encontrar?
+                </Typography>
+                <Paper sx={{ display: 'flex', justifyContent: 'center', padding: theme.spacing(0.4, 0.4, 0.4, 1.4), marginTop: '20px' }}>
+                    <InputBase
+                        placeholder="Ex.: iPhone 12 com garantia"
+                        fullWidth
+                    />
+                    <IconButton>
+                        <Search />
+                    </IconButton>
+                </Paper>
+            </Container>
+            <Container maxWidth='lg' sx={{  }}>
+                <Typography component='h2' variant="h4" align="center" color='textPrimary'>
+                    Destaques
+                </Typography>
+                <br />
+                <Grid container spacing={4}>
+                <Grid item xs={12} sm={6} md={4}>
+                        <Card>
+                            <CardMedia 
+                                image={'https://source.unsplash.com/random'}
+                                title="Título da imagem"
+                                sx={{ paddingTop: '56%' }}
+                            />
+                            <CardContent>
+                                <Typography variant='h5' component='h2'>
+                                Produto X
+                                </Typography>
+                                <Typography>
+                                R$ 60,00
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Card>
+                            <CardMedia 
+                                image={'https://source.unsplash.com/random'}
+                                title="Título da imagem"
+                                sx={{ paddingTop: '56%' }}
+                            />
+                            <CardContent>
+                                <Typography variant='h5' component='h2'>
+                                Produto X
+                                </Typography>
+                                <Typography>
+                                R$ 60,00
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Card>
+                            <CardMedia 
+                                image={'https://source.unsplash.com/random'}
+                                title="Título da imagem"
+                                sx={{ paddingTop: '56%' }}
+                            />
+                            <CardContent>
+                                <Typography variant='h5' component='h2'>
+                                Produto X
+                                </Typography>
+                                <Typography>
+                                R$ 60,00
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
+            </Container>
+        </TemplateDefault>
+    )
+}
+
+export default Home
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,74 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import {
+  AppBar, 
+  Box, 
+  Toolbar,
+  Typography,
+  Button,
+  Container,
+  IconButton,
+  Avatar,
+  Menu,
+  MenuItem,
+  Divider,
+} from '@mui/material'
+
+import Link from 'next/link';
+
+import style from '../../styles/situational.module.css'
+import { AccountCircle } from '@mui/icons-material';
 
 export default function ButtonAppBar() {
+  const [anchorUserMenu, setAnchorUserMenu] = React.useState(false)
+  
+  const openUserMenu = Boolean(anchorUserMenu)
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <Box>
+      <AppBar position="static" elevation={3}>
+        <Container maxWidth='lg'>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
+          <Link href='/' className={style.headerTitle}>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Mktp Store
+            </Typography>
+          </Link>
+          <Link href='/user/publish' className={style.headerBtnLink}>
+            <Button color="inherit" variant='outlined'>
+              Anunciar e Vender
+            </Button>
+          </Link>
+          <IconButton color='secondary' onClick={(e) => setAnchorUserMenu(e.currentTarget)}>
+            {
+              true === false
+              ? <Avatar src='' />
+              : <AccountCircle />
+            }
+            <Typography variant='subtitle2' color='secondary' sx={{ marginLeft: '6px' }}>
+              Marcos Silva
+            </Typography>
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
+
+          <Menu
+            open={openUserMenu}
+            anchorEl={anchorUserMenu}
+            onClose={() => setAnchorUserMenu(null)}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center'
+            }}
+          >
+            <Link href='/user/dashboard' className={style.userMenuLinks}>
+              <MenuItem>Meus anúncios</MenuItem>
+            </Link>
+            <Link href='/user/publish' className={style.userMenuLinks}>
+              <MenuItem>Publicar novo anúncio</MenuItem>
+            </Link>
+            <Divider sx={{ margin: '8px 0' }} />
+            <MenuItem>Sair</MenuItem>
+          </Menu>
+        </Toolbar>      
+        </Container>
       </AppBar>
     </Box>
   );
